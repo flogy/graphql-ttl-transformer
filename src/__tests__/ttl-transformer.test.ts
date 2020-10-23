164879d9726e14f64e8bb2818e96281b231ad5cd
Florian Gyger <gygy1992@hotmail.com>
2020-10-23 13:44:42 +0200

    added some more tests which are still failing at the moment

--- src/__tests__/ttl-transformer.test.ts
+++ src/__tests__/ttl-transformer.test.ts
@@ -16,9 +16,9 @@ const transformer = new GraphQLTransform({
 test("@ttl directive can be used on fields", () => {
   const schema = `
     type ExpiringChatMessage @model {
-        id: ID!
-        message: String
-        expirationUnixTime: Int! @ttl
+      id: ID!
+      message: String
+      expirationUnixTime: Int! @ttl
     }
   `;
   expect(() => transformer.transform(schema)).not.toThrow();
@@ -27,12 +27,47 @@ test("@ttl directive can be used on fields", () => {
 test("@ttl directive can not be used on types", () => {
   const schema = `
     type ExpiringChatMessage @model @ttl {
-        id: ID!
-        message: String
-        expirationUnixTime: Int!
+      id: ID!
+      message: String
+      expirationUnixTime: Int!
     }
   `;
   expect(() => transformer.transform(schema)).toThrowError(
     'Directive "ttl" may not be used on OBJECT.'
   );
+});
+
+test("Only one @ttl directive per type is allowed", () => {
+  const schema = `
+    type ExpiringChatMessage @model {
+      id: ID!
+      message: String
+      expirationUnixTime: Int! @ttl
+      anotherExpirationUnixTime: Int! @ttl
+    }
+  `;
+  expect(() => transformer.transform(schema)).toThrowError(
+    'Directive "ttl" must be used only once in the same type.'
+  );
+});
+
+test("@ttl directive can only be used on fields of type Int", () => {
+  const schema = `
+    type ExpiringChatMessage @model {
+      id: ID!
+      message: String
+      expirationUnixTime: String! @ttl
+    }
+  `;
+  expect(() => transformer.transform(schema)).toThrowError(
+    'Directive "ttl" must be used only on Int type fields.'
+  );
+});
+
+test("Generated CloudFormation document contains the TimeToLiveSpecification property", () => {
+  throw new Error("Not yet implemented");
+});
+
+test("TimeToLiveSpecification property is pointing to the field where the @ttl directive was used", () => {
+  throw new Error("Not yet implemented");
 });
